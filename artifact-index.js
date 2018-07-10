ab6190d5779b097af49b8262e321b9c42c7dd4d7
Andres Junge <ajunge@yaykuy.cl>
2018-07-10 02:26:15 -0400

    upgraded artifact index and version to 0.2.1

--- artifact-index.js
+++ artifact-index.js
@@ -2,5 +2,9 @@ module.exports = {
     AllowanceRegistry: {
       latestVersion: 'v1',
       v1: require('./build/contracts/AllowanceRegistry.json'),
-    }
+    },
+    DespachoReceta: {
+      latestVersion: 'v1',
+      v1: require('./build/contracts/DespachoReceta.json'),
+    },
 }
