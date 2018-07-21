02e33e16844c645cb75dae567d33c6967a3efdf6
Andres Junge <ajunge@yaykuy.cl>
2018-07-21 18:16:49 -0400

    despacho receta

--- contracts/DespachoReceta.sol
+++ contracts/DespachoReceta.sol
@@ -1,15 +1,18 @@
 pragma solidity 0.4.21;
 
+import "./AllowanceRegistryInterface.sol";
+import "./RestrictedRegistryInterface.sol";
+
 contract DespachoReceta {
     
     //Version del contrato
     uint256 public version = 1;
     
-    //Direccion del registro de farmacos 
-    address public registroControlados;
+    //Registro de farmacos restringidos
+    RestrictedRegistryInterface public registroControlados;
     
-    //Direccion del registro de despachadores 
-    address public registroDespachadores;
+    //Registro de despachadores 
+    AllowanceRegistryInterface public registroDespachadores;
 
     //Cantidad de farmaco recetado. CodigoFarmaco => Cantidad recetada
     mapping (bytes32 => uint256) public recetado;
@@ -22,8 +25,8 @@ contract DespachoReceta {
     function DespachoReceta(
         bytes32[] _codigoFarmaco, 
         uint256[] _cantidadRecetada,
-        address _registroControlados,
-        address _registroDespachadores
+        RestrictedRegistryInterface _registroControlados,
+        AllowanceRegistryInterface _registroDespachadores
         ) public {
             
         for(uint256 i;i<_codigoFarmaco.length;i++) {
@@ -34,7 +37,7 @@ contract DespachoReceta {
         registroDespachadores = _registroDespachadores;
     }
     
-    //["0x1","0x2"],[1,1],"0xca35b7d915458ef540ade6068dfe2f44e8fa733c","0xca35b7d915458ef540ade6068dfe2f44e8fa733c"
+    //["0x1","0x2"],[1,1],"0x789142640746970fe5569e754bc0fdb5e43b7c4f","0xb0d99cee37caac92d8aa15df69fa687f730cd04d"
 
     //Despachar 
     function despachar(
@@ -45,11 +48,13 @@ contract DespachoReceta {
         ) public {
         
         //Se requiere que el despachador este registrado 
-        //require(registroDespachadores.isAllowed(msg.sender));
+        require(registroDespachadores.isAllowed(msg.sender));
         
         //Se require que la cantidad despachada no sea mayor a la cantidad recetada.
-        //Ojo. Solo si el farmaco es controlado.
-        require(recetado[_codigoFarmaco] > despachado[_codigoFarmaco]);
+        //Solo si el farmaco es controlado.
+        if(registroControlados.isRestricted(_codigoFarmaco)){
+            require(recetado[_codigoFarmaco] > despachado[_codigoFarmaco]);
+        }
         
         //Guardar cantidad despachada
         despachado[_codigoFarmaco] += _cantidadDespachada;
